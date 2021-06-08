c3f694caa8fa33dd964976a65e20f722ae6afabd
Diego Souza <lltasunall@gmail.com>
2021-06-08 07:49:07 -0300

    Finalizando o desafio

--- assets/js/modulos/carrega_projeto.js
+++ assets/js/modulos/carrega_projeto.js
@@ -6,7 +6,6 @@ import { mostraMensagem } from "./mostra-mensagem.js"
   if (localStorage.length > 0) {
     for (let i = 0; i < localStorage.length; i++) {
       let novoProjeto = JSON.parse(localStorage.getItem(i));
-      // criaProjeto(novoProjeto);
       criaProjeto(novoProjeto)
 
       let codeContent = document.querySelectorAll("code");
